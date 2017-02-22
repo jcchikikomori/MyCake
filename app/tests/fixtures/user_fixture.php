cc2f730d2f39fb3cb3326b205f69ce8eea136285
EngagiaLocalServer <EngagiaLocalServer>
2017-02-22 18:46:50 +0800

    Beautiful Landing route :p

---
+++ app/tests/fixtures/user_fixture.php
@@ -0,0 +1,25 @@
+<?php
+/* User Fixture generated on: 2017-02-22 11:36:06 : 1487759766 */
+class UserFixture extends CakeTestFixture {
+	var $name = 'User';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'username' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 15, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'roles' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 15, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'indexes' => array(),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'username' => 'Lorem ipsum d',
+			'password' => 'Lorem ipsum dolor sit amet',
+			'roles' => 'Lorem ipsum d'
+		),
+	);
+}
